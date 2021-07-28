77a155821abcbe4a4fd75a89d02572b868658a58
bhutsul <bogdanhgf56@gmail.com>
2021-07-29 00:04:48 +0300

    main info about product, regexp and parse table

--- app/Feeds/Vendors/ULI/Parser.php
+++ app/Feeds/Vendors/ULI/Parser.php
@@ -3,11 +3,81 @@
 namespace App\Feeds\Vendors\ULI;
 
 use App\Feeds\Parser\HtmlParser;
+use App\Feeds\Utils\ParserCrawler;
+use App\Helpers\FeedHelper;
 use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
 {
+    public const DIMENSIONS_REGEXES = [
+        'WDH' => '/(\d+[\.]?\d*)\sx\s*(\d+[\.]?\d*)\sx\s*(\d+[\.]?\d*)["]?/i',
+        'WH' => '/(\d+[\/]?\d*)[",\']?\sx\s*(\d+[\/]?\d*)[",\']?/i',
+    ];
+
     private array $product_info = [];
+
+    /**
+     *product dims pattern
+     */
+    private function pushDimsToProduct(): void
+    {
+        if ( !isset( $this->product_info[ 'name' ] ) ) {
+            return;
+        }
+
+        if ( preg_match( self::DIMENSIONS_REGEXES['WDH'], $this->product_info[ 'name' ] ) ) {
+            $dims = ['desc' => $this->product_info[ 'name' ], 'regex' => [self::DIMENSIONS_REGEXES['DWH']], 'x' => 1, 'y' => 3, 'z' => 2];
+        }
+        else if ( preg_match( self::DIMENSIONS_REGEXES['WH'], $this->product_info[ 'name' ] ) ) {
+            $dims = ['desc' => $this->product_info[ 'name' ], 'regex' => [self::DIMENSIONS_REGEXES['WH']], 'x' => 1, 'y' => 2, 'z' => 3];
+        }
+
+        if ( !isset( $dims ) ) {
+            return;
+        }
+
+        $dims = FeedHelper::getDimsRegexp( $dims['desc'], $dims['regex'], $dims['x'], $dims['y'], $dims['z'] );
+
+        $this->product_info['depth']  = $dims['z'];
+        $this->product_info['height'] = $dims['y'];
+        $this->product_info['width']  = $dims['x'];
+    }
+
+    /**
+     * @return void
+     */
+    private function pushProductAttributeValues(): void
+    {
+        if ( !$this->exists( 'table#tdChart' ) ) {
+            return;
+        }
+
+        $table = $this->filter( 'table#tdChart tr' );
+
+        $table_header  = $table->first();
+        $next_elements = $table_header->nextAll();
+        $prices_values = $next_elements->first();
+        $table_values  = $prices_values->nextAll();
+
+        $table_values->each( function ( ParserCrawler $c ) use ( &$attributes, $table_header, $table ) {
+            // -- < -- because i ignore last el add to cart of table
+            for ( $i = 1; $i < $table->count(); $i++ ) {
+                $key = $table_header->filter( 'td' )->getNode( $i );
+                $value = $c->filter( 'td' )->getNode( $i );
+
+                if ( isset( $key ) && isset( $value ) ) {
+                    if ( stripos( $key->textContent, 'MODEL' )
+                        || stripos( $key->textContent, 'DIMENSIONS' )
+                        || stripos( $key->textContent, 'COLOR' )
+                        || stripos( $key->textContent, 'HEIGHT' )
+                    ) {
+                        continue;
+                    }
+                    $this->product_info['attributes'][$key->textContent] = $value->textContent;
+                }
+            }
+        });
+    }
 
     public function beforeParse(): void
     {
@@ -16,6 +86,9 @@ class Parser extends HtmlParser
             $product_info = $matches[ 1 ];
 
             $this->product_info = json_decode( $product_info, true, 512, JSON_THROW_ON_ERROR );
+
+            $this->pushDimsToProduct();
+            $this->pushProductAttributeValues();
         }
     }
 
@@ -69,5 +142,20 @@ class Parser extends HtmlParser
             'http://schema.org/InStock',
             'InStock'
         ] ) ? self::DEFAULT_AVAIL_NUMBER : 0;
+    }
+
+    public function getDimX(): ?float
+    {
+        return $this->product_info['width'] ?? null;
+    }
+
+    public function getDimY(): ?float
+    {
+        return $this->product_info['height'] ?? null;
+    }
+
+    public function getDimZ(): ?float
+    {
+        return $this->product_info['depth'] ?? null;
     }
 }
--- app/Feeds/Vendors/ULI/Vendor.php
+++ app/Feeds/Vendors/ULI/Vendor.php
@@ -4,17 +4,23 @@ namespace App\Feeds\Vendors\ULI;
 
 use App\Feeds\Processor\SitemapHttpProcessor;
 use App\Feeds\Utils\Data;
-use App\Feeds\Utils\Link;
-use App\Feeds\Utils\ParserCrawler;
 use Illuminate\Support\Facades\Storage;
 
 class Vendor extends SitemapHttpProcessor
 {
     protected array $first = ['https://www.uline.com/sitemap-us-product.xml.gz'];
-    protected const USE_PROXY = true;
-    protected const CHUNK_SIZE = 300;
-
-//    public array $custom_products = ['https://www.uline.com/Product/Detail/H-1003/Pallet-Trucks/BT-Pallet-Truck-48-x-27'];
+//    protected const USE_PROXY = true;
+//    protected const CHUNK_SIZE = 300;
+//    protected ?int $max_products = 10;
+//
+//    public array $custom_products = [
+//        'https://www.uline.com/Product/Detail/H-1003/Pallet-Trucks/BT-Pallet-Truck-48-x-27',
+//        'https://www.uline.com/Product/Detail/H-1006-NOSE/Hand-Trucks/Nose-Plate-for-Magliner-Hand-Truck-18-x-7-1-2',
+//        'https://www.uline.com/Product/Detail/S-10588P/Bubble-Rolls/Bubble-Mask-Roll-24-x-125-1-2-Perforate',
+//        'https://www.uline.com/Product/Detail/H-6761-54A/Industrial-Wire-Shelving/Chrome-Wire-Shelving-Add-On-Unit-60-x-30-x-54',
+//        'https://www.uline.com/Product/Detail/H-1006-NOSE/Hand-Trucks/Nose-Plate-for-Magliner-Hand-Truck-18-x-7-1-2',
+//        'https://www.uline.com/Product/Detail/S-21079-M/Nitrile-Gloves/Uline-Black-Industrial-Nitrile-Gloves-in-a-Bucket-Medium?model=S-21079-M&RootChecked=yes',
+//    ];
 
     public function getProductsLinks( Data $data, string $url ): array
     {
