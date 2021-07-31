3a44998dc7d0bffee1e62148380d2e654f57bfc6
bhutsul <bogdanhgf56@gmail.com>
2021-07-31 18:12:42 +0300

    validation fix

--- app/Feeds/Vendors/LFN/Vendor.php
+++ app/Feeds/Vendors/LFN/Vendor.php
@@ -17,6 +17,13 @@ class Vendor extends SitemapHttpProcessor
 
     protected function isValidFeedItem(FeedItem $fi ): bool
     {
-        return !empty( $fi->getMpn() ) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
+        if ( $fi->isGroup() ) {
+            $fi->setChildProducts( array_values(
+                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+            ) );
+            return count( $fi->getChildProducts() );
+        }
+
+        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
     }
 }
