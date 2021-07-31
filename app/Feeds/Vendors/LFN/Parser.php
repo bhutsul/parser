2fc8b7024d165aa5d5996f7399d73bb210fc546c
bhutsul <bogdanhgf56@gmail.com>
2021-07-31 16:46:56 +0300

    round size

--- app/Feeds/Vendors/LFN/Parser.php
+++ app/Feeds/Vendors/LFN/Parser.php
@@ -8,7 +8,12 @@ use App\Helpers\FeedHelper;
 
 class Parser extends ShopifyParser
 {
-    public const DIMENSIONS_REGEX = '/(\d{1,3})[\',"]?[\s]?x[\s]?(\d{1,3})[\',"]?/i';
+    public const DIMENSIONS_REGEX = '/(\d{1,3})[\',"]?[\sx\s]?(?:\d{1,3})[\',"]?/i';
+
+    protected function checkIfRound( string $description ): bool|int
+    {
+        return preg_match( '/(\d{1,3}[\.]?\d*)[\',"]?\sround/i', $description );
+    }
 
     public function beforeParse(): void
     {
@@ -19,7 +24,7 @@ class Parser extends ShopifyParser
             $dims = FeedHelper::getDimsRegexp( $this->meta[ 'title' ], [self::DIMENSIONS_REGEX] );
 
             $this->meta[ 'width' ]  = $dims[ 'x' ];
-            $this->meta[ 'height' ] = $dims[ 'y' ];
+            $this->meta[ 'height' ] = $this->checkIfRound( $this->meta[ 'title' ] ) ? $dims[ 'x' ] : $dims[ 'y' ];
         }
     }
 
@@ -35,9 +40,11 @@ class Parser extends ShopifyParser
 
     public function getChildProducts( FeedItem $parent_fi ): array
     {
-        return array_map( static function (FeedItem $item ) {
-            $dims = FeedHelper::getDimsRegexp( $item->getProduct(), [self::DIMENSIONS_REGEX] );
-            $item->setDimY( $dims[ 'y' ] );
+        return array_map( function (FeedItem $item ) {
+            $description = $item->getProduct();
+            $dims = FeedHelper::getDimsRegexp( $description, [self::DIMENSIONS_REGEX] );
+
+            $item->setDimY( $this->checkIfRound( $description ) ? $dims[ 'x' ] : $dims[ 'y' ] );
             $item->setDimX( $dims[ 'x' ] );
 
             return $item;
