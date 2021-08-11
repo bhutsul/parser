eb29be18cabd6c5a3b4b61e3d828f02440f04529
Bogdan <bogdanhgf56gmail.com>
2021-08-11 09:38:41 +0300

    validation fix

--- app/Feeds/Vendors/OLH/Vendor.php
+++ app/Feeds/Vendors/OLH/Vendor.php
@@ -23,16 +23,6 @@ class Vendor extends SitemapHttpProcessor
         }
 
         return !empty( $fi->getMpn() ) && count( $fi->getImages() );
-    }    protected function isValidFeedItem(FeedItem $fi ): bool
-    {
-        if ( $fi->isGroup() ) {
-            $fi->setChildProducts( array_values(
-                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
-            ) );
-            return count( $fi->getChildProducts() );
-        }
-
-        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
     }
 
 }
