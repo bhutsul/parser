64876be466d44ad75b1cb797ec2e75ae0088c9bb
Bogdan <bogdanhgf56gmail.com>
2021-08-11 09:38:11 +0300

    validation

--- app/Feeds/Vendors/OLH/Vendor.php
+++ app/Feeds/Vendors/OLH/Vendor.php
@@ -13,26 +13,26 @@ class Vendor extends SitemapHttpProcessor
      * @param FeedItem $fi
      * @return bool
      */
-//    protected function isValidFeedItem(FeedItem $fi ): bool
-//    {
-//        if ( $fi->isGroup() ) {
-//            $fi->setChildProducts( array_values(
-//                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
-//            ) );
-//            return count( $fi->getChildProducts() );
-//        }
-//
-//        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
-//    }    protected function isValidFeedItem(FeedItem $fi ): bool
-//    {
-//        if ( $fi->isGroup() ) {
-//            $fi->setChildProducts( array_values(
-//                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
-//            ) );
-//            return count( $fi->getChildProducts() );
-//        }
-//
-//        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
-//    }
+    protected function isValidFeedItem(FeedItem $fi ): bool
+    {
+        if ( $fi->isGroup() ) {
+            $fi->setChildProducts( array_values(
+                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+            ) );
+            return count( $fi->getChildProducts() );
+        }
+
+        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
+    }    protected function isValidFeedItem(FeedItem $fi ): bool
+    {
+        if ( $fi->isGroup() ) {
+            $fi->setChildProducts( array_values(
+                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+            ) );
+            return count( $fi->getChildProducts() );
+        }
+
+        return !empty( $fi->getMpn() ) && count( $fi->getImages() );
+    }
 
 }
