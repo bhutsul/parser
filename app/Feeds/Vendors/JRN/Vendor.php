7bc12139d04fc5d0cb51d07757a1c0c354fe9561
Bogdan <bogdanhgf56gmail.com>
2021-08-13 14:02:06 +0300

    last fixes

--- app/Feeds/Vendors/JRN/Vendor.php
+++ app/Feeds/Vendors/JRN/Vendor.php
@@ -8,15 +8,6 @@ use App\Feeds\Processor\SitemapHttpProcessor;
 class Vendor extends SitemapHttpProcessor
 {
     protected array $first = ['https://www.jracenstein.com/sitemaps/sitemap_products.xml'];
-    public array $custom_products = [
-        'https://www.jracenstein.com/p/soft-wash-nozzle-tip/150-0m',
-        'https://www.jracenstein.com/p/descent-control-window-cleaners-chair-sky-genie/92-21m',
-        'https://www.jracenstein.com/p/unger-micro-wipe/273-1m/',
-        'https://www.jracenstein.com/p/sky-genie-descender-large-1-2in-rope/93-214',
-        'https://www.jracenstein.com/p/sprayer-system-50-gallon-w-150ft-hose/150-0420',
-        'https://www.jracenstein.com/p/protool-clever-spraying-system/150-0759',
-        'https://www.jracenstein.com/p/wagtail-squeegee-aluminum-slimline/02-7m'
-    ];
 
     /**
      * @param FeedItem $fi
