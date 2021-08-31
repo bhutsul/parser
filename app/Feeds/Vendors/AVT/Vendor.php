ec9e10b2615fb3f5d9633febd073ce000f1075d9
Bogdan <bogdanhgf56gmail.com>
2021-08-31 10:45:47 +0300

    code refactor

--- app/Feeds/Vendors/AVT/Vendor.php
+++ app/Feeds/Vendors/AVT/Vendor.php
@@ -8,10 +8,8 @@ use App\Feeds\Processor\SitemapHttpProcessor;
 class Vendor extends SitemapHttpProcessor
 {
     protected array $first = ['https://www.myartventure.com/store-products-sitemap.xml'];
-    /**
-     * @param FeedItem $fi
-     * @return bool
-     */
+    public array $custom_products = ['https://www.myartventure.com/product-page/maz-6440rr'];
+
     protected function isValidFeedItem(FeedItem $fi ): bool
     {
         return !empty( $fi->getMpn() ) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
