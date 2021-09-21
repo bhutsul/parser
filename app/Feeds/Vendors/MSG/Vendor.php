215569ec9e39797c512ea7671728343432e53835
Bogdan <bogdanhgf56gmail.com>
2021-09-21 15:53:57 +0300

    categories

--- app/Feeds/Vendors/MSG/Vendor.php
+++ app/Feeds/Vendors/MSG/Vendor.php
@@ -8,12 +8,6 @@ use App\Feeds\Processor\SitemapHttpProcessor;
 class Vendor extends SitemapHttpProcessor
 {
     protected array $first = ['https://www.miamiwholesalesunglasses.com/xml-sitemap.ashx'];
-    public array $custom_products = [
-//        'https://www.miamiwholesalesunglasses.com/sunglass-display---style-tr-16c.aspx',
-//        'https://www.miamiwholesalesunglasses.com/black-sunglass-bag-pouch-h.aspx',
-//        'https://www.miamiwholesalesunglasses.com/giselle-sunglasses---8gsl22320.aspx',
-        'https://www.miamiwholesalesunglasses.com/sunglass-display---style-tr-16c.aspx',
-    ];
 
     protected function isValidFeedItem(FeedItem $fi ): bool
     {
