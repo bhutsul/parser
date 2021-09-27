ebd90cbfb0e27349a72fd6b5f3bf11b748f0a421
Bogdan <bogdanhgf56gmail.com>
2021-09-27 17:26:47 +0300

    start PBS

---
+++ app/Feeds/Vendors/PBS/Parser.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Feeds\Vendors\PBS;
+
+use App\Feeds\Parser\HtmlParser;
+use App\Feeds\Utils\ParserCrawler;
+use App\Helpers\StringHelper;
+
+class Parser extends HtmlParser
+{
+    private array $product_info;
+
+    public const NOT_VALID_PARTS_OF_DESC = [
+        'Instagram',
+        'Facebook',
+    ];
+
+    private function descriptionIsValid( string $text ): bool
+    {
+        foreach ( self::NOT_VALID_PARTS_OF_DESC as $str ) {
+            if ( false !== stripos( $text, $str ) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function beforeParse(): void
+    {
+        preg_match( '/<script type="application\/ld\+json">\s*({.*?})\s*<\/script>/s', $this->node->html(), $matches );
+        if ( isset( $matches[ 1 ] ) ) {
+            $this->product_info = json_decode( $matches[ 1 ], true, 512, JSON_THROW_ON_ERROR );
+            $this->product_info[ 'description' ] = '';
+            $this->filter( '#productDescription p' )->each( function ( ParserCrawler $c ) {
+                $text = $c->text();
+                if ( $text && $this->descriptionIsValid( $text ) ) {
+                    if ( str_contains( $text, ':' ) ) {
+                        [ $key, $value ] = explode( ':', $text, 2 );
+                        $this->product_info[ 'attributes' ][ trim( $key ) ] = trim( StringHelper::normalizeSpaceInString( $value ) );
+                    }
+                    else {
+                        $this->product_info[ 'description' ] .= '<p>' . $text . '</p>';
+                    }
+                }
+            } );
+        }
+    }
+
+    public function getProduct(): string
+    {
+        return $this->product_info[ 'name' ] ?? $this->getText( '[itemprop="name"]' );
+    }
+
+    public function getMpn(): string
+    {
+        return $this->product_info[ 'sku' ] ?? '';
+    }
+
+    public function getAvail(): ?int
+    {
+        return isset( $this->product_info[ 'availability' ] ) && $this->product_info[ 'availability' ] === 'http://schema.org/InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->product_info[ 'description' ] ?? $this->getProduct();
+    }
+
+    public function getImages(): array
+    {
+        if ( !isset( $this->product_info[ 'image' ] ) ) {
+            return [];
+        }
+        return is_array( $this->product_info[ 'image' ] ) ? $this->product_info[ 'image' ] : [ $this->product_info[ 'image' ] ];
+    }
+
+    public function getAttributes(): ?array
+    {
+        return $this->product_info[ 'attributes' ] ?? null;
+    }
+
+    public function getCostToUs(): float
+    {
+        return StringHelper::getMoney( $this->getAttr( '[itemprop="price"]', 'content' ) );
+    }
+
+    public function getCategories(): array
+    {
+        return array_values( array_slice( $this->getContent( '.ec-breadcrumbs a' ), 2, -1 ) );
+    }
+
+    public function getDimX(): ?float
+    {
+        return $this->product_info[ 'dims' ][ 'x' ] ?? null;
+    }
+
+    public function getDimY(): ?float
+    {
+        return $this->product_info[ 'dims' ][ 'y' ] ?? null;
+    }
+
+    public function getDimZ(): ?float
+    {
+        return $this->product_info[ 'dims' ][ 'z' ] ?? null;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->product_info[ 'weight' ] ?? null;
+    }
+}
---
+++ app/Feeds/Vendors/PBS/Vendor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Feeds\Vendors\PBS;
+
+use App\Feeds\Feed\FeedItem;
+use App\Feeds\Processor\SitemapHttpProcessor;
+use App\Feeds\Utils\Link;
+
+class Vendor extends SitemapHttpProcessor
+{
+    protected array $first = ['https://pricebusters.furniture/sitemap.xml'];
+
+    public array $custom_products = ['https://pricebusters.furniture/1657-Harlow-Ash-2Pc-Sofa-Love-p387712734'];
+
+//    protected function isValidFeedItem(FeedItem $fi ): bool
+//    {
+//        if ( $fi->isGroup() ) {
+//            $fi->setChildProducts( array_values(
+//                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) && $item->getCostToUs() > 0 )
+//            ) );
+//            return count( $fi->getChildProducts() );
+//        }
+//
+//        return !empty( $fi->getMpn() ) && count( $fi->getImages() ) && $fi->getCostToUs() > 0;
+//    }
+}
