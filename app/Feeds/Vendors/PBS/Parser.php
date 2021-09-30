20023eca76ceabb7657a123eabf8e7ce420389f3
Bogdan <bogdanhgf56gmail.com>
2021-09-30 15:48:00 +0300

    PBS almost done

--- app/Feeds/Vendors/PBS/Parser.php
+++ app/Feeds/Vendors/PBS/Parser.php
@@ -4,6 +4,8 @@ namespace App\Feeds\Vendors\PBS;
 
 use App\Feeds\Feed\FeedItem;
 use App\Feeds\Parser\HtmlParser;
+use App\Feeds\Utils\ParserCrawler;
+use App\Helpers\FeedHelper;
 use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
@@ -14,8 +16,20 @@ class Parser extends HtmlParser
         'No Credit Needed',
         'Click Banner',
     ];
+    public const NOT_VALID_ATTRIBUTE_VALUES = [
+        'Dimensions',
+        'End Table',
+        'Overall Dimension',
+    ];
+    public const NOT_VALID_PRODUCTS = [
+        'Not Shippable',
+        'No Shipping',
+        'not available',
+    ];
     public const REQUEST_PAYLOAD = '1cc57b09497262e8bab57ecc8dd9af46-0!722a387c~attempt1*7|1|7|https://app.ecwid.com/|BF7357332C74F21A3FF282EDEECCAE15|_|getOriginalProduct|4d|I|Z|1|2|3|4|3|5|6|7|0|%s|0|';
     public const SHIPPING_WEIGHT_KEY = 'Overall Gross Weight';
+    public const WEIGHT_KEY = 'Overall Net Weight';
+    public const ARRAY_OF_WEIGHT_ELEMENTS = [ self::SHIPPING_WEIGHT_KEY . ':', self::WEIGHT_KEY . ':' ];
 
     private function descriptionIsValid( string $text ): bool
     {
@@ -30,47 +44,83 @@ class Parser extends HtmlParser
 
     public function beforeParse(): void
     {
-        preg_match( '/<script type="application\/ld\+json">\s*({.*?})\s*<\/script>/s', $this->node->html(), $matches );
-        if ( isset( $matches[ 1 ] ) ) {
-            $this->product_info = json_decode( $matches[ 1 ], true, 512, JSON_THROW_ON_ERROR );
-            $this->product_info[ 'description' ] = '';
+        for ( $i = 0; $i <= 2; $i++ ) {
+            preg_match( '/<script type="application\/ld\+json">\s*({.*?})\s*<\/script>/s', $this->node->html(), $matches );
+            if ( isset( $matches[ 1 ] ) ) {
+                $this->product_info = json_decode( $matches[ 1 ], true, 512, JSON_THROW_ON_ERROR );
+                $this->product_info[ 'description' ] = '';
 
-            $description = preg_replace( [ '/<\w+.*?>/', '/<\/\w+>/' ], "\n", $this->getHtml( '#productDescription' ) );
-            $parts_of_description = explode( "\n", StringHelper::normalizeSpaceInString( $description ) );
+                $description = preg_replace( [ '/<\w+.*?>/', '/<\/\w+>/' ], "\n", $this->getHtml( '#productDescription' ) );
+                $parts_of_description = explode( "\n", StringHelper::normalizeSpaceInString( $description ) );
 
-            if ( $parts_of_description ) {
-                foreach ( $parts_of_description as $key_desc => $text ) {
-                    if ( $text && $this->descriptionIsValid( $text ) ) {
-                        if ( str_contains( $text, ':' ) ) {
-                            [ $key, $value ] = explode( ':', $text, 2 );
+                if ( $parts_of_description ) {
+                    foreach ( $parts_of_description as $key_desc => $text ) {
+                        if ( $text && $this->descriptionIsValid( $text ) ) {
+                            if ( str_contains( $text, ':' ) ) {
+                                [ $key, $value ] = explode( ':', $text, 2 );
 
-                            if ( empty( $value ) ) {
-                                if ( isset( $parts_of_description[ $key_desc + 1 ] ) && false !== stripos( $key, self::SHIPPING_WEIGHT_KEY ) ) {
-                                    $this->product_info[ 'shipping_weight' ] = StringHelper::getFloat( $parts_of_description[ $key_desc + 1 ] );
+                                if ( empty( trim( $value ) ) ) {
+                                    if ( isset( $parts_of_description[ $key_desc + 1 ] ) ) {
+                                        if ( false !== stripos( $key, self::SHIPPING_WEIGHT_KEY ) ) {
+                                            $this->product_info[ 'shipping_weight' ] = StringHelper::getFloat( $parts_of_description[ $key_desc + 1 ] );
+                                            continue;
+                                        }
+
+                                        if ( false !== stripos( $key, self::WEIGHT_KEY ) ) {
+                                            $this->product_info[ 'weight' ] = StringHelper::getFloat( $parts_of_description[ $key_desc + 1 ] );
+                                            continue;
+                                        }
+                                    }
+
+                                    foreach ( self::NOT_VALID_ATTRIBUTE_VALUES as $str ) {
+                                        if ( false !== stripos( $key, $str ) ) {
+                                            continue 2;
+                                        }
+                                    }
+
+                                    $this->product_info[ 'description' ] .= '<p>' . $text . '</p>';
+                                }
+                                else {
+                                    switch ( $key ) {
+                                        case 'Height':
+                                            $this->product_info[ 'dims' ][ 'y' ] = StringHelper::getFloat( $value );
+                                            break;
+                                        case 'Width':
+                                            $this->product_info[ 'dims' ][ 'x' ] = StringHelper::getFloat( $value );
+                                            break;
+                                        case 'Depth':
+                                            $this->product_info[ 'dims' ][ 'z' ] = StringHelper::getFloat( $value );
+                                            break;
+                                        case 'Weight':
+                                            $this->product_info[ 'weight' ] = StringHelper::getFloat( $value );
+                                            break;
+                                        case 'Dimensions':
+                                            $this->product_info[ 'dims' ] = FeedHelper::getDimsInString( $value, 'x', 0, 2, 1 );
+                                            break;
+                                        case self::SHIPPING_WEIGHT_KEY:
+                                            $this->product_info[ 'shipping_weight' ] = StringHelper::getFloat( $value );
+                                            break;
+                                        default:
+                                            $this->product_info[ 'attributes' ][ trim( $key ) ] = trim( StringHelper::normalizeSpaceInString( $value ) );
+                                            break;
+                                    }
+                                }
+                            }
+                            else {
+                                if ( isset( $parts_of_description[ $key_desc - 1 ] ) && in_array( $parts_of_description[ $key_desc - 1 ], self::ARRAY_OF_WEIGHT_ELEMENTS ) ) {
                                     continue;
                                 }
 
                                 $this->product_info[ 'description' ] .= '<p>' . $text . '</p>';
                             }
-                            else {
-                                if ( false !== stripos( $key, self::SHIPPING_WEIGHT_KEY ) ) {
-                                    $this->product_info[ 'shipping_weight' ] = StringHelper::getFloat( $value );
-                                    continue;
-                                }
-
-                                $this->product_info[ 'attributes' ][ trim( $key ) ] = trim( StringHelper::normalizeSpaceInString( $value ) );
-                            }
-                        }
-                        else {
-                            if ( isset( $parts_of_description[ $key_desc - 1 ] ) && $parts_of_description[ $key_desc - 1 ] === self::SHIPPING_WEIGHT_KEY . ':' ) {
-                                continue;
-                            }
-
-                            $this->product_info[ 'description' ] .= '<p>' . $text . '</p>';
                         }
                     }
                 }
+
+                break;
             }
+
+            $this->node = new ParserCrawler( $this->getVendor()->getDownloader()->get( $this->getUri() )->getData() );
         }
     }
 
@@ -78,16 +128,34 @@ class Parser extends HtmlParser
     {
         preg_match( '/p(\d+)/', $this->getUri(), $payload_match );
         if ( isset( $payload_match[ 1 ] ) ) {
-            $this->getVendor()->getDownloader()->setHeader( 'Content-Type', 'text/x-gwt-rpc; charset=UTF-8' );
-            $data = $this->getVendor()->getDownloader()->post( 'https://app.ecwid.com/rpc?ownerid=47528127&customerlang=en&version=2021-37865-g11365a50609', [
-                sprintf( self::REQUEST_PAYLOAD, $payload_match[ 1 ] ),
-            ], 'raw_data' )->getData();
+            $version_href = $this->getAttr( '[as="script"]', 'href' );
+            $version_regex = '/' . date( 'Y' ) . '\/(.*?)\//';
+            $owner_href = $this->getAttr( '[property="og:image"]', 'content' );
+            $owner_regex = '/images\/(.*?)\//';
 
-            if ( false !== stripos( $data, 'Not Shippable' ) || false !== stripos( $data, 'No Shipping' ) ) {
-                $fi->setCostToUs( 0 );
-                $fi->setRAvail( 0 );
-                $fi->setMpn( '' );
-                $fi->setImages( [] );
+            preg_match( $version_regex, $version_href, $version_match );
+            preg_match( $owner_regex, $owner_href, $owner_match );
+
+            if ( isset( $version_match[ 1 ], $owner_match[ 1 ] ) ) {
+                $this->getVendor()->getDownloader()->setHeader( 'Content-Type', 'text/x-gwt-rpc; charset=UTF-8' );
+
+                $link = 'https://app.ecwid.com/rpc?';
+                $link .= 'ownerid=' . $owner_match[ 1 ];
+                $link .= '&customerlang=en&';
+                $link .= 'version=' . $version_match[ 1 ];
+
+                $data = $this->getVendor()->getDownloader()->post( $link, [
+                    sprintf( self::REQUEST_PAYLOAD, $payload_match[ 1 ] ),
+                ], 'raw_data' )->getData();
+
+                foreach ( self::NOT_VALID_PRODUCTS as $str ) {
+                    if ( false !== stripos( $data, $str ) ) {
+                        $fi->setCostToUs( 0 );
+                        $fi->setRAvail( 0 );
+                        $fi->setMpn( '' );
+                        $fi->setImages( [] );
+                    }
+                }
             }
         }
     }
@@ -104,12 +172,12 @@ class Parser extends HtmlParser
 
     public function getAvail(): ?int
     {
-        return isset( $this->product_info['offers'][ 'availability' ] ) && $this->product_info['offers'][ 'availability' ] === 'http://schema.org/InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
+        return isset( $this->product_info[ 'offers' ][ 'availability' ] ) && $this->product_info[ 'offers' ][ 'availability' ] === 'http://schema.org/InStock' ? self::DEFAULT_AVAIL_NUMBER : 0;
     }
 
     public function getDescription(): string
     {
-        return $this->product_info[ 'description' ] ?? $this->getProduct();
+        return !empty( $this->product_info[ 'description' ] ) ? $this->product_info[ 'description' ] : $this->getProduct();
     }
 
     public function getImages(): array
@@ -130,9 +198,19 @@ class Parser extends HtmlParser
         return StringHelper::getMoney( $this->getAttr( '[itemprop="price"]', 'content' ) );
     }
 
-    public function getCategories(): array
+    public function getDimX(): ?float
     {
-        return array_values( array_slice( $this->getContent( '.ec-breadcrumbs a' ), 2, -1 ) );
+        return $this->product_info[ 'dims' ][ 'x' ] ?? null;
+    }
+
+    public function getDimY(): ?float
+    {
+        return $this->product_info[ 'dims' ][ 'y' ] ?? null;
+    }
+
+    public function getDimZ(): ?float
+    {
+        return $this->product_info[ 'dims' ][ 'z' ] ?? null;
     }
 
     public function getWeight(): ?float
--- app/Feeds/Vendors/PBS/Vendor.php
+++ app/Feeds/Vendors/PBS/Vendor.php
@@ -8,6 +8,7 @@ use App\Feeds\Processor\SitemapHttpProcessor;
 class Vendor extends SitemapHttpProcessor
 {
     protected array $first = ['https://pricebusters.furniture/sitemap.xml'];
+    public array $custom_products = ['https://pricebusters.furniture/2038-Soft-Touch-Leather-Match-2Pc-Sectional-p387710932'];
 
     protected function isValidFeedItem(FeedItem $fi ): bool
     {
