9344ff1fc9a46d93bdbc271c73ad4fecd3b4becd
Bogdan <bogdanhgf56gmail.com>
2021-10-11 17:29:15 +0300

    options

--- app/Feeds/Vendors/JMF/Parser.php
+++ app/Feeds/Vendors/JMF/Parser.php
@@ -2,6 +2,7 @@
 
 namespace App\Feeds\Vendors\JMF;
 
+use App\Feeds\Feed\FeedItem;
 use App\Feeds\Parser\HtmlParser;
 use App\Feeds\Utils\ParserCrawler;
 use App\Helpers\FeedHelper;
@@ -9,8 +10,6 @@ use App\Helpers\StringHelper;
 
 class Parser extends HtmlParser
 {
-    private array $product_info;
-
     public const NOT_VALID_PARTS_OF_DESC_REGEXES = [
         '/<div\b[^>]+\bclass=[\'\"]prod-faq[\'\"][^>]*>(.*?)<\/div>/s',
     ];
@@ -20,6 +19,8 @@ class Parser extends HtmlParser
         'Net weight',
         'Net Dimensions',
     ];
+
+    private array $product_info;
 
     private function formattedImages( array $images ): array
     {
@@ -61,6 +62,9 @@ class Parser extends HtmlParser
                 else if ( isset( $json[ '[data-gallery-role=gallery-placeholder]' ][ 'Magento_ProductVideo/js/fotorama-add-video-events' ] ) ) {
                     $this->product_info[ 'videos' ] = $this->formattedVideos( $json[ '[data-gallery-role=gallery-placeholder]' ][ 'Magento_ProductVideo/js/fotorama-add-video-events' ][ 'videoData' ] );
                 }
+                else if ( isset( $json[ '[data-role=swatch-options]' ][ 'Magento_Swatches/js/swatch-renderer' ][ 'jsonConfig' ] ) ) {
+                    $this->product_info[ 'swatch' ] = $json[ '[data-role=swatch-options]' ][ 'Magento_Swatches/js/swatch-renderer' ][ 'jsonConfig' ];
+                }
             }
         }
     }
@@ -69,7 +73,7 @@ class Parser extends HtmlParser
     {
         $this->filter( '.mp-attachment-tab a' )->each( function ( ParserCrawler $a ) {
             $this->product_info[ 'files' ][] = [
-                'name' => $a->text(),
+                'name' => preg_replace( '/[\s+]?\(\d+[.]?\d*.*?\)/u', '', $a->text() ),
                 'link' => $a->attr( 'href' ),
             ];
         } );
@@ -107,7 +111,7 @@ class Parser extends HtmlParser
                 $this->pushWeight( $key, $value );
 
                 if ( false !== stripos( $key, 'Net Dimensions (W x D x H)' ) ) {
-                    $this->product_info['dims'] = FeedHelper::getDimsInString($value, 'x', 0, 2,1);
+                    $this->product_info[ 'dims' ] = FeedHelper::getDimsInString( $value, 'x', 0, 2, 1 );
                 }
 
                 if ( $this->isAttributeValid( $key ) ) {
@@ -124,11 +128,168 @@ class Parser extends HtmlParser
         } );
     }
 
+    private function combinations( array $arrays, int $i = 0 )
+    {
+        if ( !isset( $arrays[ $i ] ) ) {
+            return [];
+        }
+
+        if ( $i === count( $arrays ) - 1 ) {
+            return $arrays[ $i ];
+        }
+
+        // get combinations from subsequent arrays
+        $tmp = $this->combinations( $arrays, $i + 1 );
+
+        $result = [];
+
+        // concat each array from tmp with each element from $arrays[$i]
+        foreach ( $arrays[ $i ] as $v ) {
+            foreach ( $tmp as $t ) {
+                $result[] = is_array( $t ) ? array_merge( [ $v ], $t ) : [ $v, $t ];
+            }
+        }
+
+        return $result;
+    }
+
+    private function childClone(
+        FeedItem $parent_fi,
+        array &$child,
+        string $name,
+        string $mpn,
+        int|float $price,
+        null|array $images
+    ): void
+    {
+        $fi = clone $parent_fi;
+
+        $fi->setProduct( $name );
+        $fi->setCostToUs( $this->getCostToUs() + $price );
+        $fi->setRAvail( $this->getAvail() );
+        $fi->setImages( $images ?? $this->getImages() );
+
+        $fi->setDimX( $this->getDimX() );
+        $fi->setDimY( $this->getDimY() );
+        $fi->setDimZ( $this->getDimZ() );
+
+        $fi->setWeight( $this->getWeight() );
+
+        $fi->setMpn( $mpn );
+
+        $child[] = $fi;
+    }
+
+    private function getChildNameAndMpnAndPriceAndImagesInGroup( array $option_values, array $options ): array
+    {
+        $images = [];
+
+        [ $name, $mpn, $price ] = $this->getDefaultNameMpnPrice();
+
+        foreach ( $option_values as $option_value ) {
+            $this->prepareNameAndMpnAndPrice( $options[ $option_value ], $name, $mpn, $price );
+
+            if ( isset( $option[ 'images' ] ) ) {
+                $images = array_merge( $options[ $option_value ][ 'images' ], $images );
+            }
+        }
+
+        return [ $name, $mpn, $price, $images ? $this->formattedImages( $images ) : null ];
+    }
+
+    private function prepareNameAndMpnAndPrice(
+        array $option,
+        string &$name,
+        string &$mpn,
+        int|float|string &$price
+    ): void
+    {
+        $name .= $option[ 'name' ];
+        $name .= ': ';
+        $name .= $option[ 'value' ];
+
+        $mpn .= '-';
+        $mpn .= $option[ 'id' ];
+
+        $price += $option[ 'price' ];
+    }
+
+    private function getDefaultNameMpnPrice(): array
+    {
+        return [ '', $this->getMpn(), 0 ];
+    }
+
+    private function getOptionsAndGroups(): array
+    {
+        $options = [];
+        $option_groups = [];
+
+        if ( isset( $this->product_info[ 'swatch' ] ) ) {
+            foreach ( $this->product_info[ 'swatch' ][ 'attributes' ] as $attribute ) {
+                $option_values = [];
+
+                foreach ( $attribute[ 'options' ] as $option ) {
+                    if ( $option[ 'products' ] ) {
+                        $options[ $option[ 'id' ] ] = [
+                            'name' => $attribute[ 'label' ],
+                            'value' => $option[ 'label' ],
+                            'price' => 0,
+                            'id' => $option[ 'id' ],
+                        ];
+
+                        if ( isset( $this->product_info[ 'swatch' ][ 'index' ] ) ) {
+                            foreach ( $this->product_info[ 'swatch' ][ 'index' ] as $key => $options_index ) {
+                                if (
+                                    isset( $options_index[ $attribute[ 'id' ] ], $this->product_info[ 'swatch' ][ 'images' ][ $key ] )
+                                    && $options_index[ $attribute[ 'id' ] ] === $option[ 'id' ]
+                                ) {
+                                    $options[ $option[ 'id' ] ][ 'images' ] = $this->product_info[ 'swatch' ][ 'images' ][ $key ];
+
+                                    break;
+                                }
+                            }
+                        }
+                        $option_values[] = $option[ 'id' ];
+                    }
+                }
+
+                if ( $option_values ) {
+                    $option_groups[] = $option_values;
+                }
+            }
+        }
+
+        $this->filter( '#product-options-wrapper select' )
+            ->each( function ( ParserCrawler $select ) use ( &$options, &$option_groups ) {
+                if ( false !== stripos( $select->attr( 'class' ), 'required' ) ) {
+                    $option_values = [];
+
+                    $select->filter( 'option' )
+                        ->each( function ( ParserCrawler $option ) use ( &$options, &$option_values, $select ) {
+                            if ( $option->attr( 'value' ) ) {
+                                $options[ $option->attr( 'value' ) ] = [
+                                    'name' => $this->getText( 'label[for="' . $select->attr( 'id' ) . '"]' ),
+                                    'value' => $option->text(),
+                                    'price' => $option->attr( 'price' ),
+                                    'id' => $option->attr( 'value' ),
+                                ];
+                                $option_values[] = $option->attr( 'value' );
+                            }
+                        } );
+                    if ( $option_values ) {
+                        $option_groups[] = $option_values;
+                    }
+                }
+            } );
+
+        return [ $options, $option_groups ];
+    }
+
     public function beforeParse(): void
     {
         $sku = $this->getAttr( '[data-bv-show="rating_summary"]', 'data-bv-product-id' );
         $this->product_info = [
-            'name' => preg_replace('/[\s+]?[-]?[\s+]?[#]?[\s+]?'. $sku . '/ui','',$this->getText( '[itemprop="name"]' )),
+            'name' => preg_replace( '/[\s+]?[-]?[\s+]?[#]?[\s+]?' . $sku . '/ui', '', $this->getText( '[itemprop="name"]' ) ),
             'price' => StringHelper::getFloat( $this->getAttr( '[itemprop="price"]', 'content' ) ),
             'sku' => $sku,
             'categories' => $this->getContent( '.items .category a' ),
@@ -142,6 +303,11 @@ class Parser extends HtmlParser
         $this->pushDimsAndAttributes();
 
         $this->pushShorts();
+    }
+
+    public function isGroup(): bool
+    {
+        return $this->exists( '#product-options-wrapper select' ) || !empty( $this->product_info[ 'swatch' ][ 'attributes' ] );
     }
 
     public function getProduct(): string
@@ -191,17 +357,17 @@ class Parser extends HtmlParser
 
     public function getDimX(): ?float
     {
-        return $this->product_info['dims']['x'] ?? null;
+        return $this->product_info[ 'dims' ][ 'x' ] ?? null;
     }
 
     public function getDimY(): ?float
     {
-        return $this->product_info['dims']['y'] ?? null;
+        return $this->product_info[ 'dims' ][ 'y' ] ?? null;
     }
 
     public function getDimZ(): ?float
     {
-        return $this->product_info['dims']['z'] ?? null;
+        return $this->product_info[ 'dims' ][ 'z' ] ?? null;
     }
 
     public function getWeight(): ?float
@@ -212,5 +378,37 @@ class Parser extends HtmlParser
     public function getAvail(): ?int
     {
         return self::DEFAULT_AVAIL_NUMBER;
+    }
+
+    public function getChildProducts( FeedItem $parent_fi ): array
+    {
+        $child = [];
+
+        [ $options, $option_groups ] = $this->getOptionsAndGroups();
+
+        if ( count( $option_groups ) === 1 ) {
+            foreach ( $options as $option ) {
+                [ $name, $mpn, $price ] = $this->getDefaultNameMpnPrice();
+
+                if ( isset( $option[ 'images' ] ) ) {
+                    $images = $this->formattedImages( $option[ 'images' ] );
+                }
+
+                $this->prepareNameAndMpnAndPrice( $option, $name, $mpn, $price );
+
+                $this->childClone( $parent_fi, $child, $name, $mpn, $price, $images ?? null );
+            }
+        }
+        else {
+            $combination_of_groups = $this->combinations( $option_groups );
+
+            foreach ( $combination_of_groups as $option_values ) {
+                [ $name, $mpn, $price, $images ] = $this->getChildNameAndMpnAndPriceAndImagesInGroup( $option_values, $options );
+
+                $this->childClone( $parent_fi, $child, $name, $mpn, $price, $images );
+            }
+        }
+
+        return $child;
     }
 }
--- app/Feeds/Vendors/JMF/Vendor.php
+++ app/Feeds/Vendors/JMF/Vendor.php
@@ -7,11 +7,18 @@ use App\Feeds\Processor\SitemapHttpProcessor;
 
 class Vendor extends SitemapHttpProcessor
 {
-    protected array $first = ['https://www.justrite.com/pub/sitemap/sitemap.xml'];
+    protected array $first = [ 'https://www.justrite.com/pub/sitemap/sitemap.xml' ];
     protected const DELAY_S = 0.1;
 
-    protected function isValidFeedItem(FeedItem $fi ): bool
+    protected function isValidFeedItem( FeedItem $fi ): bool
     {
+        if ( $fi->isGroup() ) {
+            $fi->setChildProducts( array_values(
+                array_filter( $fi->getChildProducts(), static fn( FeedItem $item ) => !empty( $item->getMpn() ) && count( $item->getImages() ) )
+            ) );
+            return count( $fi->getChildProducts() );
+        }
+
         return !empty( $fi->getMpn() ) && count( $fi->getImages() );
     }
 }
